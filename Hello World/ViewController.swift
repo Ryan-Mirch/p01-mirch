16bc6bd208fec0a16eae950fad2961544c197852
Ryan Mirch <rwmirch@gmail.com>
2019-01-29 21:42:29 -0500

    Added button that changes labels color

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var theButton: UIButton!    
+    @IBOutlet weak var theLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        theLabel.textColor = randomColor()
+    }
+    
+    func randomColor() -> UIColor {
+        let redValue = 0.01 * Float(Int.random(in:0 ..< 100))
+        let blueValue = 0.01 * Float(Int.random(in:0 ..< 100))
+        let greenValue = 0.01 * Float(Int.random(in:0 ..< 100))
+    
+        let result = UIColor(red: CGFloat(redValue),
+                             green: CGFloat(greenValue),
+                             blue: CGFloat(blueValue), alpha: 1)
+        return result;
+    }
+    
 
 }
 
